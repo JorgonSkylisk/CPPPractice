48a85e971e6869e0b02e309eece0fece9942157c
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-09-05 14:44:52 +0100

    Added message

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,16 @@
 //library
 #include <stdlib.h> // standard library
-
+#include <iostream> //input and output library
 
 //entry function REQUIRED for C++
 
 int main()
 {
+	std::cout << "CHEEKI BREEKI IV DAMKE!" << std::endl;
+
+
 	// Stops program from continuing until a key press happens
 	system("PAUSE");
-
 
 
 	// tell the ooperating system we are done and no error
