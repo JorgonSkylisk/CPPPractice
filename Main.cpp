cfc035ed0c8a1c8758153f40b1a3bb10ce0caadd
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-09-05 14:38:30 +0100

    Added libray and Pause

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,16 @@
+//library
+#include <stdlib.h> // standard library
+
+
 //entry function REQUIRED for C++
 
 int main()
 {
+	// Stops program from continuing until a key press happens
+	system("PAUSE");
+
+
+
 	// tell the ooperating system we are done and no error
 	return 0;
 }
